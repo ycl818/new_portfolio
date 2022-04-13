60b90c4dc31b64c9b71ca411d5e1a6251c964220
yichien <kevinyichienlee@gmail.com>
2022-04-14 00:25:03 +0800

    update header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -46,28 +46,36 @@ const Header = () => {
       whileInView={{ opacity: [0, 1] }}
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
-    >
-      <img src={images.profile} alt="profile_bg" />
-      <motion.img
+      >
+
+        <motion.div
+          whileInView={{ scale: [0, 1.05] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className="app__header-profile"
+        >
+          <img src={images.profile} alt="profile_bg" />
+        </motion.div>
+      
+      {/* <motion.img
         whileInView={{ scale: [0, 1.1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
         src={images.circle}
         alt="profile_circle"
         className="overlay_circle"
-      />
-    </motion.div>
+      /> */}
+      </motion.div>
 
-    <motion.div
-      variants={scaleVariants}
-      whileInView={scaleVariants.whileInView}
-      className="app__header-circles"
-    >
-      {[images.flutter, images.redux, images.sass].map((circle, index) => (
-        <div className="circle-cmp app__flex" key={`circle-${index}`}>
-          <img src={circle} alt="profile_bg" />
-        </div>
-      ))}
-    </motion.div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="profile_bg" />
+          </div>
+        ))}
+      </motion.div>
     </div>
   )
 }
---
+++ frontend_react/src/wrapper/MotionWrap.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+
+const MotionWrap = () => {
+  return (
+    <div>MotionWrap</div>
+  )
+}
+
+export default MotionWrap
