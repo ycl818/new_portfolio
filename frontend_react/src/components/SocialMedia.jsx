7586f7cab41f2e81964b1a1c327e645ee81ace38
yichien <kevinyichienlee@gmail.com>
2022-04-15 19:54:28 +0800

    update media link

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import { BsInstagram } from 'react-icons/bs'
+import { FaFacebookF,  FaLinkedinIn } from 'react-icons/fa'
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsTwitter />
+      <div>   
+        <a href='https://www.instagram.com/qq_lyc0818/' target="_blank"><BsInstagram /></a>
       </div>
+
       <div>
-        <FaFacebookF />
+        <a href='https://www.facebook.com/kevinyichienlee/'  target="_blank"><FaFacebookF /></a>
       </div>
+
       <div>
-        <BsInstagram />
+        <a href='https://www.linkedin.com/in/yi-chienlee/'  target="_blank"> < FaLinkedinIn /></a>
       </div>
     </div>
   )
