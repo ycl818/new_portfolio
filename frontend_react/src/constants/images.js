2a24a490dc6b1e1b675bcbf4f57c11d6663e371b
yichien <kevinyichienlee@gmail.com>
2022-04-19 21:21:26 +0800

    remove import from image.js

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,4 +1,4 @@
-import email from '../assets/email.png';
+
 import mail from '../assets/mail.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
@@ -10,7 +10,7 @@ import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
-import mu5 from '../assets/mu5.png';
+
 import node from '../assets/node.png';
 import python from '../assets/python.png';
 import react from '../assets/react.png';
@@ -19,10 +19,7 @@ import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
+
 
 import profile from '../assets/profile.jpg';
 import profile2 from '../assets/profile2.jpg'
@@ -30,19 +27,16 @@ import profile3 from '../assets/profile3.jpg'
 import profile4 from '../assets/profile4.jpg'
 import profile5 from '../assets/profile5.jpg'
 import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
+
+
+
 import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
+
 import spotify from '../assets/spotify.png';
 
 export default {
   mail,
-  email,
   mobile,
   api,
   cpp,
@@ -53,7 +47,6 @@ export default {
   graphql,
   html,
   javascript,
-  mu5,
   node,
   python,
   react,
@@ -61,22 +54,12 @@ export default {
   sass,
   typescript,
   vue,
-  about01,
-  about02,
-  about03,
-  about04,
   profile,
   profile2,
   profile3,
   profile4,
   profile5,
   circle,
-  logo,
-  adidas,
-  amazon,
   asus,
-  bolt,
-  nb,
-  skype,
   spotify,
 };
