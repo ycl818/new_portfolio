007faee6c44f1d434fc7d839e29ab5277d5361a2
yichien <kevinyichienlee@gmail.com>
2022-04-21 01:14:11 +0800

    update work sorting

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -13,7 +13,7 @@ const Work = () => {
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
-    const query = '*[_type == "works"]';
+    const query = '*[_type == "works"] | order(title asc)';
 
     client.fetch(query).then((data) => {
       
